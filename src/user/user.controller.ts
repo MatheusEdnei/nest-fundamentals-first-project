f7b65045ee79e9cbd97483d3b54f95ebeacb4b38
Matheus Ednei <ednei880@gmail.com>
2023-09-27 15:53:04 -0300

    feat(decorator): add custom decorator ParamId

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,6 +15,7 @@ import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
+import { ParamId } from 'src/decorators/param-id.decorator';
 // import { CacheInterceptor } from 'src/interceptors/cache.interceptor';
 
 @UseInterceptors(LogInterceptor)
@@ -33,28 +34,26 @@ export class UserController {
     }
 
     @Get(':id')
-    async readOne(@Param('id', ParseIntPipe) id: number) {
+    async readOne(@ParamId() id: number) {
+        console.log({ id });
         return await this.userService.show(id);
     }
 
     @Put(':id')
-    async update(
-        @Body() data: UpdatePutUserDTO,
-        @Param('id', ParseIntPipe) id: number,
-    ) {
+    async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
         return await this.userService.update(id, data);
     }
 
     @Patch(':id')
     async updateParcial(
         @Body() data: UpdatePatchUserDTO,
-        @Param('id', ParseIntPipe) id: number,
+        @ParamId() id: number,
     ) {
         return await this.userService.updatePartial(id, data);
     }
 
     @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id: number) {
+    async delete(@ParamId() id: number) {
         return await this.userService.delete(id);
     }
 }
