9f3e82ebe805410802fbcf87f4dee33a9219d0b0
Matheus Ednei <ednei880@gmail.com>
2024-03-14 14:05:58 -0300

    feat: add custom decorator to RBAC

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,6 +16,8 @@ import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
 import { UserService } from './user.service';
 import { LogInterceptor } from 'src/interceptors/log.interceptor';
 import { ParamId } from 'src/decorators/param-id.decorator';
+import { Roles } from 'src/decorators/rules.decorator';
+import { Role } from 'src/enums/role.enum';
 // import { CacheInterceptor } from 'src/interceptors/cache.interceptor';
 
 @UseInterceptors(LogInterceptor)
@@ -23,27 +25,32 @@ import { ParamId } from 'src/decorators/param-id.decorator';
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    @Roles(Role.Admin)
     @Post()
     async create(@Body() data: CreateUserDTO) {
         return await this.userService.create(data);
     }
 
+    @Roles(Role.Admin)
     @Get()
     async read() {
         return await this.userService.list();
     }
 
+    @Roles(Role.Admin)
     @Get(':id')
     async readOne(@ParamId() id: number) {
         console.log({ id });
         return await this.userService.show(id);
     }
 
+    @Roles(Role.Admin)
     @Put(':id')
     async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
         return await this.userService.update(id, data);
     }
 
+    @Roles(Role.Admin)
     @Patch(':id')
     async updateParcial(
         @Body() data: UpdatePatchUserDTO,
@@ -52,6 +59,7 @@ export class UserController {
         return await this.userService.updatePartial(id, data);
     }
 
+    @Roles(Role.Admin)
     @Delete(':id')
     async delete(@ParamId() id: number) {
         return await this.userService.delete(id);
