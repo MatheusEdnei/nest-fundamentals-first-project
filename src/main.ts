6285e073c18685370fc4f2a90468cfe7ba33946c
Matheus Ednei <ednei880@gmail.com>
2024-05-13 14:27:51 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ['hcode.com.br'],
+    });
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new LogInterceptor());
     await app.listen(3000);
