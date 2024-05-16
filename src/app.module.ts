507d66c0f7860d81f85b72a37362156afe7057ff
Matheus Ednei <ednei880@gmail.com>
2024-05-16 16:11:48 -0300

    feat: use throttler module in app module to limit request

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-    imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+    imports: [
+        forwardRef(() => UserModule),
+        forwardRef(() => AuthModule),
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60000,
+                limit: 10,
+                ignoreUserAgents: [/googlebot/gi],
+            },
+        ]),
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule {}
