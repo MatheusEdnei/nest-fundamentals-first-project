db1dc6483434063740b42900ec74b2e2c0ce19de
Matheus Ednei <ednei880@gmail.com>
2024-05-16 16:19:45 -0300

    feat: use config and process.env to get environment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthService } from './auth.service';
 @Module({
     imports: [
         JwtModule.register({
-            secret: 'TXJebJz9pU6pPVZ6QjksdLNUyNhRTtXz',
+            secret: process.env.JWT_SECRET,
         }),
         forwardRef(() => UserModule),
         PrismaModule,
